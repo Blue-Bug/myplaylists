f364bab8190ee89875ea539dec3788013d239f1b
Blue-Bug <kjj9987@gmail.com>
2021-12-25 17:32:13 +0900

    Member Entity v1

---
+++ src/main/java/com/myplaylists/domain/Member.java
@@ -0,0 +1,36 @@
+package com.myplaylists.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity @EqualsAndHashCode(of="id")
+@Getter @Setter @Builder
+@AllArgsConstructor @NoArgsConstructor
+public class Member {
+    @Id @GeneratedValue
+    private Long id;
+
+    private String nickname;
+
+    private String email;
+
+    private String password;
+
+    private String token;
+
+    private LocalDateTime joinedAt;
+
+    private LocalDateTime tokenGeneratedAt;
+
+    private boolean emailVerified;
+
+    public void generateToken() {
+        this.token = UUID.randomUUID().toString();
+        this.tokenGeneratedAt = LocalDateTime.now();
+    }
+}
