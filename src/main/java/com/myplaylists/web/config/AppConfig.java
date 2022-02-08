338714e25c0137c42d0029b17aee7bce177339af
Blue-Bug <kjj9987@gmail.com>
2022-02-08 18:39:28 +0900

    heroku domain name redirect

--- src/main/java/com/myplaylists/web/config/AppConfig.java
+++ src/main/java/com/myplaylists/web/config/AppConfig.java
@@ -1,13 +1,27 @@
 package com.myplaylists.web.config;
 
+import com.myplaylists.web.interceptor.URLInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class AppConfig {
+@RequiredArgsConstructor
+public class AppConfig implements WebMvcConfigurer {
+    private final AppProperties appProperties;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new URLInterceptor(appProperties))
+                .order(1)
+                .addPathPatterns("/**");
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
---
+++ src/main/java/com/myplaylists/web/interceptor/URLInterceptor.java
@@ -0,0 +1,35 @@
+package com.myplaylists.web.interceptor;
+
+import com.myplaylists.web.config.AppProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RequiredArgsConstructor
+public class URLInterceptor implements HandlerInterceptor {
+
+    private final AppProperties appProperties;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String requestURI = request.getRequestURI();
+        if(requestURI.contains("herokuapp")){
+            response.sendRedirect(appProperties.getHost());
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
